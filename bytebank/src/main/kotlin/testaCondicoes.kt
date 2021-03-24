9ce309d9f3c10312b48954916c941bd7cd3fbf14
raphaelarm0103 <75501869+raphaelarm0103@users.noreply.github.com>
2021-03-24 15:37:11 -0300

    teste

--- bytebank/src/main/kotlin/testaCondicoes.kt
+++
@@ -1,7 +0,0 @@
-fun testaCondicoes(saldo: Double) {
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> println("conta é negativa")
-    }
-}
